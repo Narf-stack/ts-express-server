22e20bca6c06481beaa3a7f5e29bb7b2d38f361b
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-09 20:51:42 +0100

    Basic Routes with Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('h')
+import express, {Request, Response} from 'express';
+
+
+const app = express()
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send(`
+    <div>
+        <h1> Oye ! </h1>
+    </div>
+    `
+    )
+})
+
+
+app.listen(3000,()=>{
+    console.log('Listenning on 3000')
+})

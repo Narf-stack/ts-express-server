f0d067ca7dd7fdcaa63a4577197b1005317c085e
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-09 21:20:05 +0100

    Routes - get/post loggin + Parsing Form Bodies

--- build/index.js
+++ build/index.js
@@ -4,10 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes/loginRoutes");
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = (0, express_1.default)();
-app.get('/', function (req, res) {
-    res.send("\n    <div>\n        <h1> Oye ! </h1>\n    </div>\n    ");
-});
+// app.get('/', (req:Request, res:Response)=>{
+//     res.send(`
+//     <div>
+//         <h1> Oye ! </h1>
+//     </div>
+//     `
+//     )
+// })
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(loginRoutes_1.router);
 app.listen(3000, function () {
     console.log('Listenning on 3000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import express, {Request, Response} from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser  from 'body-parser';
 
 
 const app = express()
 
-app.get('/', (req:Request, res:Response)=>{
-    res.send(`
-    <div>
-        <h1> Oye ! </h1>
-    </div>
-    `
-    )
-})
-
-
+// app.get('/', (req:Request, res:Response)=>{
+//     res.send(`
+//     <div>
+//         <h1> Oye ! </h1>
+//     </div>
+//     `
+//     )
+// })
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(router)
 app.listen(3000,()=>{
     console.log('Listenning on 3000')
 })

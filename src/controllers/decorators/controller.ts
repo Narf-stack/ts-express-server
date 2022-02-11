dda4fd2b82a09a84b63b4d7739ad60a227dcb44b
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-11 16:59:03 +0100

    Routes - Metadata Keys

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import express from 'express'
 import { AppRouter } from '../../AppRouter'
 import { Methods } from './Methods'
+import {MetadataKeys} from './MetadataKeys'
 
 
 export function controller(routePrefix:string){
@@ -9,8 +10,8 @@ export function controller(routePrefix:string){
         const router = AppRouter.getInstance()
         for(let key in target.prototype){
             const routeHandler = target.prototype[key] // method
-            const path = Reflect.getMetadata('path',target.prototype, key)
-            const method:Methods = Reflect.getMetadata('method',target.prototype, key)
+            const path = Reflect.getMetadata(MetadataKeys.path,target.prototype, key)
+            const method:Methods = Reflect.getMetadata(MetadataKeys.method,target.prototype, key)
 
             if(path){
                 router[method](`${routePrefix}${path}`,routeHandler)
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,14 +1,14 @@
 import 'reflect-metadata'
 import {Methods} from './Methods'
-
+import {MetadataKeys} from './MetadataKeys'
 
 // To avoid multiply the same function again & again, creation of a routeBinder
 
 function routeBinder(method:string){
     return function (path:string){
         return function(target:any, key:string, desc: PropertyDescriptor){
-            Reflect.defineMetadata('path',path,target,key)
-            Reflect.defineMetadata('method',method,target,key)
+            Reflect.defineMetadata(MetadataKeys.path,path,target,key)
+            Reflect.defineMetadata(MetadataKeys.method,method,target,key)
         }
     }
 }
